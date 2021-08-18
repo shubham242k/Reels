ccbe431b7da9029fe876d3b0edda9ee601bf1a78
Shubham Aggarwal <aggarwalshubham2793@gmail.com>
2021-08-19 01:27:10 +0530

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app"
+const firebaseConfig = {
+    apiKey: "AIzaSyA8c5UTRh8W1y7-phJBqdEU4LNJeuGpF-A",
+    authDomain: "reels-32b5d.firebaseapp.com",
+    projectId: "reels-32b5d",
+    storageBucket: "reels-32b5d.appspot.com",
+    messagingSenderId: "384788258460",
+    appId: "1:384788258460:web:5bc8d49996504a85709759"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
